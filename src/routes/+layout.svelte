e9e79357ae36e7d07c337adbc1688809bf3eafa6
18juba <jubalucas05@gmail.com>
2026-08-20 20:30:11 -0300

    Implementando speed insights da Vercel

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,13 @@
   import favicon from "$lib/assets/favicon.png";
   import { dev } from '$app/environment';
   import { injectAnalytics } from '@vercel/analytics/sveltekit';
+  import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
   import '@fontsource-variable/google-sans/wght.css';
 
   let { children } = $props();
 
   injectAnalytics({ mode: dev ? 'development' : 'production' });
+  injectSpeedInsights();
 </script>
 
 <svelte:head>
